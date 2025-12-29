668a5bdbc1c956a859a3f8bb9c9167bb1023b54b
gaykun1 <139235847+gaykun1@users.noreply.github.com>
2025-12-29 14:34:38 +0200

    (feature):simple controller for handling n8n http request

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Body, Controller, Post, BadRequestException } from '@nestjs/common';
 
-@Controller()
+@Controller('webhook')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('analyze')
+  analyzeRequest(@Body() body: { text: string; userId: number }) {
+    console.log('Received data:', body);
+
+    if (!body.text) {
+      throw new BadRequestException('Text is required');
+    }
+
+    const isUrgent = body.text.toLowerCase().includes('now');
+    const sentiment = body.text.length > 20 ? 'Detailed' : 'Short';
+
+    // returning json file
+    return {
+      status: 'success',
+      analysis: {
+        isUrgent,
+        sentiment,
+        processedAt: new Date().toISOString(),
+        server: 'NestJS core'
+      }
+    };
   }
 }
